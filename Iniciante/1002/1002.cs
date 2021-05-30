3c54d49eb2539100c9b59617d10bdb1ed768968d
Pablo Ferreira <39393083+PabloVizard@users.noreply.github.com>
2021-05-29 22:45:57 -0300

    1000 terminado

---
+++ Iniciante/1002/1002.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _1002
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
